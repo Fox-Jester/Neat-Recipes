41e4663ee3e3a5ac4fffc3bf991ffa4fa7d5b1b0
Will Marshall <willmarshall2424@gmail.com>
2024-12-30 22:25:50 -0500

    created app.info

--- creation/creation-page.js
+++ creation/creation-page.js
@@ -6,6 +6,7 @@ const App = {
     $: {
         
         nameGroup: document.querySelector("#name-group"),
+        cardName: document.querySelector("#card-name"),
         
         
         descriptionGroup: document.querySelector("#description-group"),
@@ -19,16 +20,22 @@ const App = {
         instructionBtn: document.querySelector("#instruction-btn"),
         instructionList: document.querySelector("#instruction-list"),
 
-        prepInput: document.querySelector("#prep-input"),
+        prepInputHour: document.querySelector("#prep-input-hour"),
+        prepInputMins: document.querySelector("#prep-input-mins"),
         prepBtn: document.querySelector("#prep-btn"),
 
-        cookInput: document.querySelector("#cook-input"),
+        cookInputHour: document.querySelector("#cook-input-hour"),
+        cookInputMins: document.querySelector("#cook-input-mins"),
         cookBtn: document.querySelector("#cook-btn"),
 
         servingInput: document.querySelector("#serving-input"),
         servingBtn: document.querySelector("#serving-btn"),
 
-        imgImput: document.querySelector("#img-imput"),
+        imgInput: document.querySelector("#img-input"),
+        imagePreview: document.querySelector("#img-preview"),
+        
+
+        finishBtn: document.querySelector("#finish-btn"),
     },
 
     init(){
@@ -36,15 +43,36 @@ const App = {
             this.applyDescription()
             this.applyIngrediants()
             this.applyInstructions()
-
+            this.applyPrepTime()
+            this.applyCookTime()
+            this.applyServings()
+            this.imgChange()
         },
     
+    info: {
+        nameData: "",
+        descriptionData: "",
+        ingrediantData: "",
+        instructionData: "",
+        prepDataHour: "",
+        prepDataMins: "",
+        cookDataHour: "",
+        cookDataMins: "",
+        servingData: "",
+        imgData: "",
+
+    },
+
 
     applyName() {
         const nameInput = document.querySelector("#name-input")
         const nameBtn = document.querySelector("#name-btn")
+
+
         nameBtn.addEventListener("click", (e) => {
-            if (nameInput.value !== null){
+            if (this.validator(nameInput.value)){
+                this.$.cardName.innerHTML = nameInput.value;
+                this.info.nameData = nameInput.value;
                 nameBtn.remove();
                 nameInput.remove();
                 
@@ -55,7 +83,7 @@ const App = {
                 redoBtn.addEventListener("click", (e) => {
                     App.$.nameGroup.innerHTML = `<input type="text" id="name-input" class="input add-input"
                             placeholder="Add a Name"/> <button id="name-btn" class="btn btn-primary add-btn">ENTER</button>`
-                    this.reapplyName()
+                    this.applyName()
 
                 })
 
@@ -69,7 +97,8 @@ const App = {
         const descriptionInput = document.querySelector("#description-input");
         const descriptionBtn = document.querySelector("#description-btn");
         descriptionBtn.addEventListener("click", (e) => {
-            if (descriptionInput.value !== null){
+            if (this.validator(descriptionInput.value)){
+                this.info.descriptionData = descriptionInput.value;
                 descriptionBtn.remove();
                 descriptionInput.remove();
                 
@@ -81,7 +110,7 @@ const App = {
                 redoBtn.addEventListener("click", (e) => {
                     this.$.descriptionGroup.innerHTML = `<input type="text" id="description-input" class="input add-input"
                             placeholder="Quick description"/> <button id="description-btn" class="btn btn-primary add-btn">ENTER</button>`
-                    this.reapplyDescription();
+                    this.applyDescription();
 
                 })
 
@@ -93,7 +122,7 @@ const App = {
 
     applyIngrediants() {
         this.$.ingrediantBtn.addEventListener("click", (e) => {
-            if (this.$.ingrediantInput !== null){
+            if (this.validator(this.$.ingrediantInput.value)){
                 const wrapper = document.createElement("div")
                 wrapper.classList.add("instruction-group");
                 wrapper.innerHTML = `<p class="imput-text">${this.$.ingrediantInput.value}</p> <button class="btn  
@@ -115,7 +144,7 @@ const App = {
 
     applyInstructions() {
         this.$.instructionBtn.addEventListener("click", (e) => {
-            if (this.$.instructionArea.value !== null){
+            if (this.validator(this.$.instructionArea.value)){
                 const wrapper = document.createElement("div")
                 wrapper.classList.add("instruction-group");
                 wrapper.innerHTML = `<li class="imput-text">${this.$.instructionArea.value}</li> <button class="btn  
@@ -132,6 +161,14 @@ const App = {
             })
     },
 
+
+    validator(value){
+        const regex = /\S/;
+        
+        return regex.test(value);
+        
+    },
+
     delete() {
         try {
             this.parentElement.remove()
@@ -139,10 +176,54 @@ const App = {
         catch(err){
             
         }
+    },
+
+    applyPrepTime() {
+        this.$.prepBtn.addEventListener("click", (e) => {
+            if (this.validator(this.$.prepInputHour.value)){
+                this.info.prepDataHour = this.$.prepInputHour.value;
+            }
+            if (this.validator(this.$.prepInputMins.value)){
+                this.info.prepDataMins = this.$.prepInputMins.value;
+            }
+
+        })
+    },
+
+    applyCookTime() {
+        this.$.cookBtn.addEventListener("click", (e) => {
+            if(this.validator(this.$.cookInputHour.value)){
+                this.info.cookDataHour = this.$.cookInputHour.value;
+            }
+            if(this.validator(this.$.cookInputMins.value)){
+                this.info.cookDataMins = this.$.cookInputMins.value;
+            }
+        })
+    },
+
+    applyServings() {
+        this.$.servingBtn.addEventListener("click", (e) => {
+            if(this.validator(this.$.servingInput.value)){
+                this.info.servingData = this.$.servingInput;
+            }
+        })
+    },
+    
+    imgChange() {
+        this.$.imgInput.onchange = () => {
+            this.$.imagePreview.src = URL.createObjectURL(this.$.imgInput.files[0]);
+        }
+    },
+
+    createRecipe() {
+        this.$.finishBtn.addEventListener("click", (e) => {
+            this.info.ingrediantData = this.$.ingrediantList.innerHTML;
+            this.info.instructionData = this.$.instructionList.innerHTML;
+            this.info.imgData = this.$.imagePreview.src;
+            
+        })
+
     }
-   
-
-
 
 }
 
